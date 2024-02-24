a6fcee010473b8baef8619922d19c0bac7e6c5f2
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-02-24 16:39:03 +0900

    chapter07 Null Safety 적용

--- chapter07/android/app/src/main/kotlin/com/example/chapter07/MainActivity.kt
+++ chapter07/android/app/src/main/kotlin/com/example/chapter07/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.chapter07
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- chapter07/lib/main.dart
+++ chapter07/lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: false,
       ),
       home: MyHomePage(),
     );
